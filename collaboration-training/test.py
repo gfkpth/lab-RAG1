da30a46115573f836f3e1c8a46237d1c73899408
gfkpth <groeg3@web.de>
2025-06-24 16:29:54 +0200

    making the graph very pretty

--- collaboration-training/test.py
+++ collaboration-training/test.py
@@ -3,5 +3,7 @@ from matplotlib import pyplot as plt
 print("Hello Georg!")
 
 
-plt.plot([1,2,4],[4,5,6])
+plt.plot([1,2,4,7],[4,5,6,7])
+plt.xlabel('one type of unit')
+plt.ylabel('yet another unit')
 plt.show()
